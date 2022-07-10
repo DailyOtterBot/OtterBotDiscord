001e0ad0209c44c92010e77fe220013d1c3d8a76
KwiiHours <41028518+KwiiHours@users.noreply.github.com>
2022-07-10 15:04:28 +1200

    Fixes

--- build/events/buttonInteraction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const buttonInteraction = {
-    name: 'buttonInteraction',
-    execute: function () {
-    }
-};
-exports.default = buttonInteraction;
-//# sourceMappingURL=buttonInteraction.js.map
